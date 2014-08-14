5c56f82ccf51d60e8f6b744b61f9d2d368420145
Ben Shryock <benshry@gmail.com>
2014-08-14 00:10:59 -0700

    factored script into get_quotes function

--- data/gather.py
+++ data/gather.py
@@ -3,17 +3,18 @@ import urllib2
 from bs4 import BeautifulSoup
 import re
 
+def get_quotes(name, last_page):
+  show_quotes = []
+  for i in range(1, last_page + 1):
+    url = 'sample'
+    page = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(page)
+    page_quotes = soup.findAll('div', {'id':re.compile('quote_*')})
+    show_quotes.extend([q.get_text() for q in page_quotes])
+  return show_quotes
+
 if __name__ == '__main__':
-  all_quotes = []
-
+  all_quotes = {}
   with open('out.txt', 'w') as out:
-
-    for i in range(1, 24):
-      url = 'sample_url'
-      page = urllib2.urlopen(url).read()
-      soup = BeautifulSoup(page)
-      page_quotes = soup.findAll('div', {'id':re.compile('quote_*')})
-      all_quotes.extend([q.get_text() for q in page_quotes])
-
-    json.dumps({'archer':all_quotes})
-    json.dump({'archer':all_quotes}, out)
+    all_quotes['archer'] = get_quotes('archer', 23)
+    json.dump(all_quotes, out)
